36b4cfe9f0516bfc003d6881106cb83dd60e4323
Mykhailo Nosa <48624287+misha04873@users.noreply.github.com>
2020-12-06 02:10:08 +0200

    30%

--- src/test/java/firstOne/FirstTest.java
+++ src/test/java/firstOne/FirstTest.java
@@ -1,7 +1,14 @@
 package firstOne;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FirstTest {
 
@@ -9,10 +16,55 @@ public class FirstTest {
     public void firstTest() {
         System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe");
         ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://prestashop-automation.qatestlab.com.ua/ru/");
+
+
+// Тест 1
+// Достаем валюту с товаров
+        List<WebElement> elements = driver.findElements(By.className("price"));
+        for (WebElement element : elements) {
+            System.out.println(element.getText());
+        }
+
+// Достаем валюту с шапки сайта
+        String currency = driver.findElement(By.cssSelector("#_desktop_currency_selector > div > span.expand-more._gray-darker.hidden-sm-down")).getText();
+        System.out.println(currency);
+
+
+// Достаем символ валюты и выводим в консоль
+        String currencySymbol = currency.substring(currency.length() - 1);
+        System.out.println(currencySymbol);
+
+
+// Сверяем символ с концом на всех товарах
+        for (WebElement element : elements) {
+            assertTrue(element.getText().endsWith(currencySymbol));
+        }
+
+
+// Тест 2
+//        driver.findElement(By.cssSelector("#_desktop_currency_selector > div")).click();
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", driver.findElement(By.xpath("//*[@id=\"_desktop_currency_selector\"]/div/ul/li[3]/a")));
 
 
 
-        driver.get("https://dota2.ru/");
-        driver.quit();
+
+// Тест 3
+// Ищем dress
+        WebElement searchBox = driver.findElement(By.name("s"));
+        searchBox.sendKeys("dress");
+        driver.findElement(By.cssSelector("#search_widget > form > button")).click();
+
+
+
+
+
+
+
+
+
+
+
     }
 }
